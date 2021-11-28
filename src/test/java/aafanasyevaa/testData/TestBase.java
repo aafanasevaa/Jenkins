5c36e7d849574270250be4456151af687f0b9d47
Anastasiia <afanasyevaa.anastasiia@gmail.com>
2021-11-28 14:06:29 +0300

    added listener

--- src/test/java/aafanasyevaa/testData/TestBase.java
+++ src/test/java/aafanasyevaa/testData/TestBase.java
@@ -1,11 +1,14 @@
 package aafanasyevaa.testData;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
         Configuration.startMaximized = true;
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 }
