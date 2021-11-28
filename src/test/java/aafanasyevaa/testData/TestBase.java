891130769ab562150a6cb7fa5ea53cd35821c592
Anastasiia <afanasyevaa.anastasiia@gmail.com>
2021-11-28 14:22:58 +0300

    added attachments

--- src/test/java/aafanasyevaa/testData/TestBase.java
+++ src/test/java/aafanasyevaa/testData/TestBase.java
@@ -1,14 +1,32 @@
 package aafanasyevaa.testData;
 
+import aafanasyevaa.helpers.Attach;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
     @BeforeAll
-    static void beforeAll() {
+    static void doBeforeAll() {
         Configuration.startMaximized = true;
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @AfterEach
+    public void doAfterEach() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
